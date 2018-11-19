a5076a738f9eb7773b9db870710f00b3755f4481
Trajche Shoposki <tsoposki@gmail.com>
2018-11-19 19:45:19 +0100

    S4 & S5 & S6

--- src/app/containers/list-view/list-view.component.ts
+++ src/app/containers/list-view/list-view.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ILocation } from 'src/tests';
+import { LocationService } from 'src/app/services';
+import { shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
-  styleUrls: ['./list-view.component.scss']
+  styleUrls: ['./list-view.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ListViewComponent implements OnInit {
-  constructor() {}
+export class ListViewComponent {
+  locations$: Observable<Array<ILocation>>;
 
-  ngOnInit() {}
+  constructor(private locationService: LocationService) {
+    this.locations$ = this.locationService.getLocations().pipe(shareReplay(1));
+  }
 }
